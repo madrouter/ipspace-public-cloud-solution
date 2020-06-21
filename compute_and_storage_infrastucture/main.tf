e1020b2fb6f3e08b7b327dd8b20df3c6ee426817
Michael Haigh <mad_router@yahoo.com>
2020-06-21 15:29:05 +1000

    Find VPC by filters

--- compute_and_storage_infrastucture/main.tf
+++ compute_and_storage_infrastucture/main.tf
@@ -31,9 +31,11 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "web" {
-	ami = "${data.aws_ami.ubuntu.id}"
+	ami = data.aws_ami.ubuntu.id
 	instance_type = "t2.micro"
 	key_name = "AWS_SSH_Keypair"
+	subnet_id = data.aws_subnet.selected.id
+
 
 	tags = {
 		Name = "Web_Server"
