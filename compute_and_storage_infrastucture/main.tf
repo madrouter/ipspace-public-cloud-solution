9855a75724cc3f74554983321b8e41bccfe4ff60
Michael Haigh <mad_router@yahoo.com>
2020-06-21 17:02:12 +1000

    HTTP is port 80 - not 443

--- compute_and_storage_infrastucture/main.tf
+++ compute_and_storage_infrastucture/main.tf
@@ -37,8 +37,8 @@ resource "aws_security_group" "allow_http" {
 
 	ingress {
 		description = "HTTP from Anywhere"
-		from_port = 443
-		to_port = 443
+		from_port = 80
+		to_port = 80
 		protocol = "tcp"
 		cidr_blocks = ["0.0.0.0/0"]
 	}
